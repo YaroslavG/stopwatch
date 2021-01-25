8bd90d82c7317c8ece60592129f9b881778f3d3a
@y.gorkun <yaroslav.gorkun@gmail.com>
2021-01-25 20:03:45 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss']
+})
+
+
+export class TimerComponent implements OnInit, OnDestroy {
+  clock: any;
+  minutes: any = '00';
+  seconds: any = '00';
+  milliseconds: any = '00';
+
+  @Input() start: boolean;
+  @Input() showTimerControls: boolean;
+
+  constructor() {
+
+  }
+
+  counter: number;
+  timerRef;
+  running: boolean = false;
+  startText = 'Start';
+
+
+  startTimer() {
+    this.running = !this.running;
+    if (this.running) {
+      this.startText = 'Stop';
+      const startTime = Date.now() - (this.counter || 0);
+      this.timerRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+        this.milliseconds = Math.floor(Math.floor(this.counter % 1000) / 10).toFixed(0);
+        this.minutes = Math.floor(this.counter / 60000);
+        this.seconds = Math.floor(Math.floor(this.counter % 60000) / 1000).toFixed(0);
+        if (Number(this.minutes) < 10) {
+          this.minutes = '0' + this.minutes;
+        } else {
+          this.minutes = '' + this.minutes;
+        }
+        if (Number(this.milliseconds) < 10) {
+          this.milliseconds = '0' + this.milliseconds;
+        } else {
+          this.milliseconds = '' + this.milliseconds;
+        }
+        if (Number(this.seconds) < 10) {
+          this.seconds = '0' + this.seconds;
+        } else {
+          this.seconds = '' + this.seconds;
+        }
+      });
+    } else {
+      this.startText = 'Start';
+      this.clearTimer();
+    }
+  }
+
+  waitTimer() {
+    this.startText = 'Start';
+    this.running = !this.running;
+    clearInterval(this.timerRef);
+  }
+
+  clearTimer() {
+    this.running = false;
+    this.startText = 'Start';
+    this.counter = undefined;
+    this.milliseconds = '00',
+      this.seconds = '00',
+      this.minutes = '00';
+    clearInterval(this.timerRef);
+  }
+
+  resetTimer() {
+    this.clearTimer();
+    this.startTimer();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
+  }
+  ngOnInit() {
+  }
+
+}
